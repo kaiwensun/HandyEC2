4a18ec86335e6ea39af1b3c1fa9ccbe9ae8b7211
Kaiwen Sun <skw_kevin@126.com>
2023-07-30 11:03:12 -0700

    enable ssm

--- handy_ec2/handy_ec2_stack.py
+++ handy_ec2/handy_ec2_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
-    aws_autoscaling as autoscaling
+    aws_autoscaling as autoscaling,
+    aws_iam as iam
 )
 from constructs import Construct
 
@@ -26,15 +27,23 @@ class HandyEc2Stack(Stack):
         public_sg.add_ingress_rule(peer, ec2.Port.tcp(22), "SSH")
         public_sg.add_ingress_rule(peer, ec2.Port.tcp(3389), "Remote Desktop")
         windows = ec2.WindowsImage(ec2.WindowsVersion.WINDOWS_SERVER_2022_CHINESE_SIMPLIFIED_FULL_BASE)
+        instance_profile = iam.Role(self, "Role",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            description="Instance Profile for HandyEc2Stack",
+            role_name="HandyEc2Stack"
+        )
+        instance_profile.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedEC2InstanceDefaultPolicy"))
+
         autoscaling.AutoScalingGroup(self, "ASG",
             auto_scaling_group_name="HandyEC2ASG",
             vpc=default_vpc,
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.LARGE),
+            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.LARGE),
             machine_image=windows,
             security_group=public_sg,
             desired_capacity=1,
             min_capacity=1,
             max_capacity=1,
-            key_name=f"{self.account}-{self.region}-ec2-keypair"
+            key_name=f"{self.account}-{self.region}-ec2-keypair",
+            role=instance_profile
         )
         
